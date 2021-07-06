3dad1e2584d1652a5d22f4abf9c27937740111a1
MarcPretorius <marc@jamarservices.co.za>
2021-07-06 10:42:06 +0200

    asfkjsd

--- resources/views/themes/default1/layouts/agent.blade.php
+++ resources/views/themes/default1/layouts/agent.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Cloud TS | HELP DESK</title>
+        <title>CloudTS</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- Bootstrap 3.3.2 -->
         <link href="{{asset("lb-faveo/downloads/bootstrap.min.css")}}" rel="stylesheet" type="text/css" />
